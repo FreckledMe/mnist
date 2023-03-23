43160a8583635482a8eabbd9bf4a8ef314a176cd
Kamoliddin Jabbarov <justfrozenak@gmail.com>
2023-03-23 15:44:31 +0500

    streamlit updated

--- streamlit.py
+++ streamlit.py
@@ -2,21 +2,24 @@ import streamlit as st
 import cv2
 from tensorflow import keras
 import numpy as np
+from PIL import Image
 
 st.title('Rasmga qarab sonni topib beraman')
 
 
-model = keras.models.load_model(r'C:\Users\justf\Notebooks\mnist\model\mnist_model.h5')
-image = st.file_uploader(label='Image upload',type=['png','jpg'])
-if image:
-    # img = image.getvalue()
-    img = cv2.imread(image.name)
-    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+model = keras.models.load_model('model\mnist_model.h5')
+uploaded_image = st.file_uploader(label='Image upload',type=['png','jpg'])
+if uploaded_image:
+    file_bytes = np.asarray(bytearray(uploaded_image.read()),dtype=np.uint8)
+    cv_image = cv2.imdecode(file_bytes,1)
+    gray_img = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
     gray_img = cv2.resize(gray_img,dsize=(28,28))
     gray_img = gray_img.astype('float32')
     gray_img /= 255
     gray_img = np.array([gray_img])
-
+    for_display_image = cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
+    for_display_image = cv2.resize(for_display_image,dsize=(120,120,))
+    st.image(for_display_image)
 
 with st.form("key1"):
     # ask for input
